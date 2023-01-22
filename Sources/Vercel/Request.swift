a457c6206c7226d3609e63d96ff93146b2520c59
Andrew Barba <barba@hey.com>
2023-01-22 11:40:06 -0500

    Request helpers

--- Sources/Vercel/Request.swift
+++ Sources/Vercel/Request.swift
@@ -11,3 +11,22 @@ public struct Request: Codable, Sendable {
     public let path: String
     public let body: String?
 }
+
+extension Request {
+
+    public var id: String {
+        headers["x-vercel-id"]?.value ?? "dev1:dev1::00000-0000000000000-000000000000"
+    }
+
+    public var host: String {
+        headers["host"]?.value ?? "localhost"
+    }
+
+    public var userAgent: String {
+        headers["user-agent"]?.value ?? "unknown"
+    }
+
+    public var clientIPAddress: String {
+        headers["x-vercel-forwarded-for"]?.value ?? "127.0.0.1"
+    }
+}
