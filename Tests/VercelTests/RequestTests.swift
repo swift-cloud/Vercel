be000d23a9545f09a9dfe00728254ceab8a73e8d
Andrew Barba <barba@hey.com>
2023-01-22 14:41:09 -0500

    Multi-value header test

--- Tests/VercelTests/RequestTests.swift
+++ Tests/VercelTests/RequestTests.swift
@@ -2,11 +2,32 @@ import XCTest
 @testable import Vercel
 
 final class RequestTests: XCTestCase {
-    func testDecode() throws {
+    func testSimpleDecode() throws {
         let json = """
-        {"method": "GET", "path": "/", "headers": {}}
+        {
+          "method": "GET",
+          "path": "/",
+          "headers": {}
+        }
         """
         let req = try JSONDecoder().decode(Request.self, from: json.data(using: .utf8)!)
         XCTAssertEqual(req.method, .GET)
+    }
+
+    func testMultiValueHeaderDecode() throws {
+        let json = """
+        {
+          "method": "GET",
+          "path": "/",
+          "headers": {
+            "a": "1",
+            "b": ["2", "3"]
+          }
+        }
+        """
+        let req = try JSONDecoder().decode(Request.self, from: json.data(using: .utf8)!)
+        XCTAssertEqual(req.method, .GET)
+        XCTAssertEqual(req.headers["a"]!.value, "1")
+        XCTAssertEqual(req.headers["b"]!.value, "2")
     }
 }
