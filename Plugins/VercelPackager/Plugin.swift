912ec3ffb41b0db84d1d0fd18deff541e0bb70df
Andrew Barba <barba@hey.com>
2023-01-25 11:55:02 -0500

    Cleanup dev

--- Plugins/VercelPackager/Plugin.swift
+++ Plugins/VercelPackager/Plugin.swift
@@ -12,15 +12,23 @@ import PackagePlugin
 struct VercelPackager: CommandPlugin {
     func performCommand(context: PackagePlugin.PluginContext, arguments: [String]) async throws {
         let vercelOutput = VercelOutput(packageManager: packageManager, context: context, arguments: arguments)
+        if vercelOutput.isDev {
+            try await dev(vercelOutput)
+        } else {
+            try await build(vercelOutput)
+        }
+    }
+
+    private func build(_ vercelOutput: VercelOutput) async throws {
         try await vercelOutput.prepare()
         try await vercelOutput.build()
-        if vercelOutput.isDev {
-            try await vercelOutput.dev()
-            return
-        }
         if vercelOutput.isDeploy {
             try await vercelOutput.deploy()
             return
         }
+    }
+
+    private func dev(_ vercelOutput: VercelOutput) async throws {
+        try await vercelOutput.dev()
     }
 }
