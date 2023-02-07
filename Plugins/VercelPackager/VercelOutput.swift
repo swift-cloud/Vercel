5b723aed51d0c358d8eeadddff137b0aa340b0d8
Andrew Barba <barba@hey.com>
2023-02-07 09:56:27 -0500

    Support deprecated dev command

--- Plugins/VercelPackager/VercelOutput.swift
+++ Plugins/VercelPackager/VercelOutput.swift
@@ -118,7 +118,7 @@ extension VercelOutput {
     }
 
     public var isDev: Bool {
-        arguments.contains("dev")
+        arguments.contains("dev") || arguments.contains("--dev")
     }
 
     public var isDeploy: Bool {
