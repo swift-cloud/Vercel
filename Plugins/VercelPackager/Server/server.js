aa37fa63b01e21b3cb7cdfacad9e2915e31c27d7
Andrew Barba <barba@hey.com>
2023-08-25 15:05:44 -0400

    handle base64 encoded body

--- Plugins/VercelPackager/Server/server.js
+++ Plugins/VercelPackager/Server/server.js
@@ -54,8 +54,9 @@ const server = http.createServer(async (req, res) => {
     const body = await readBody(req)
     const _res = await invoke({ method, path, headers, body })
     const _body = JSON.parse(await readBody(_res))
+    console.log(_body)
     res.writeHead(_body.statusCode, _body.headers)
-    res.end(_body.body)
+    res.end(Buffer.from(_body.body || '', _body.isBase64Encoded ? 'base64' : 'utf8'))
   } catch (err) {
     console.error(err)
     res.writeHead(500, {})
