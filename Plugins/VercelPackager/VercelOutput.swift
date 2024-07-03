da8a4e3e7a04dcc226e038a6832c9f46efe06022
Andrew Barba <barba@hey.com>
2024-07-03 14:10:05 -0400

    Optimize for size

--- Plugins/VercelPackager/VercelOutput.swift
+++ Plugins/VercelPackager/VercelOutput.swift
@@ -563,7 +563,7 @@ extension VercelOutput {
                 "-v", "\(context.package.directory.string):/workspace",
                 "-w", "/workspace",
                 baseImage,
-                "bash", "-cl", "swift build -c release --static-swift-stdlib"
+                "bash", "-cl", "swift build -c release -Xswiftc -Osize --static-swift-stdlib"
             ]
         )
 
